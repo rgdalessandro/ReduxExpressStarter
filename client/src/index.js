aa39f93b4ef4a5e42edaaada6008bbcc3bfb0682
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2018-03-02 20:07:36 -0600

    upgrade to v2 including hot reloading

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Router, browserHistory } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
 import reducers from './reducers';
-import routes from './routes.js';
+// import routes from './routes.js';
+
+import App from './components/app/App';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory} routes={routes} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('#root'));
--- server/server.js
+++ server/server.js
@@ -5,14 +5,17 @@ const path = require('path');
 const port = process.env.PORT || 5000;
 const app = express();
 
-const assetFolder = path.join(__dirname, '../public');
+const assetFolder = path.join(__dirname, '../client/build');
 
 app.use(express.static(assetFolder));
 
+app.get('/ping', (req, res) => {
+  res.json('pong');
+});
+
 app.get('*', (req, res) => {
-   res.sendFile(path.join(assetFolder, 'index.html'));
+  res.sendFile(path.join(assetFolder, 'index.html'));
 });
 
 app.listen(port);
 console.log('Server started and listening on port', port);
-console.log('NODE_ENV:', process.env.NODE_ENV);
